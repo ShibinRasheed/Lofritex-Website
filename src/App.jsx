d9e4d5c153bff6bd186b4d7a2373c23e7842a6cc
ShibinRasheed <shibinrasheed333@gmail.com>
2024-07-09 22:11:31 +0530

    Coming Soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import Typewriter from "typewriter-effect";
 
 export default function App() {
   return (
-    <div className="bg-black text-white">
-        <h1>
-          hello 
-        </h1>
+    <div className="w-screnn h-screen flex justify-center items-center bg-black text-white ">
+      <h1 className="font-extrabold text-[50px]">
+        <Typewriter
+          options={{
+            strings: ["Your Loyal Friendly Tech Expert is almost here..."],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </h1>
     </div>
-  )
+  );
 }
